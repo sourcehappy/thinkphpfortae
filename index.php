048b6a5fc83f0f8a87363cc3cafc0da42e430050
15902047486_725164861@uz.taobao.com <15902047486_725164861@uz.taobao.com>
2013-10-26 12:34:51 +0800

    @ rename pro

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+//define('RUNTIME_ALLINONE', true);  // 开启ALLINONE运行模式
+define('APP_DEBUG', true);
+define("THINK_PATH","./ThinkPHP/");
+define("APP_NAME","Index");
+define("APP_PATH","./Index/");
+require THINK_PATH.'ThinkPHP.php';
+
+?>
